3492fb553a4ced82b3f162bcf4779e0252efde20
90Viktoriya <viktoria.kileeva@gmail.com>
2024-05-02 21:56:48 +0300

    feat: setup file aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,12 @@ export default defineConfig({
     globals: true,
     include: ["./src/**/*.test.{js, jsx, ts,tsx}", "./tests/**/*.test.{js, jsx, ts,tsx}"],
     environment: "jsdom"
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+    },
   },
 })
